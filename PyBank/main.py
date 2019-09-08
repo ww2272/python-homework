05aa3cbbd911cb6cad9f21a56ecc6a0f5754a7c8
ww2272 <weiching1991@gmail.com>
2019-09-08 13:03:50 -0400

    edited PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,33 +7,47 @@ with open("02-Homework_02-Python_Instructions_PyBank_Resources_budget_data.csv")
     minimum = 0
     maximum = 0
     next(csv_reader)
+    log = []
+    change_list = []
     for row in csv_reader:
-        month_count += 1
-        total = total + float(row[1])
-        if minimum == 0:
-            minimum = float(row[1])
-            min_name = row[0]
-        if maximum == 0:
-            maximum = float(row[1])
-            max_name = row[0]
-        elif float(row[1]) < minimum:
-            minimum = float(row[1])
-            min_name = row[0]
-        elif float(row[1]) > maximum:
-            maximum = float(row[1])
-            max_name = row[0]
-                
-    print("Financial Analysis")
-    print("--------------------")
-    print(f"Total Months: {month_count}")
-    print(f"Total: ${total}")
-    print(f"Average  Change: ${total/month_count}")
-    print(f"Greatest Increase in Profits: {max_name} (${maximum})")
-    print(f"Greatest Decrease in Profits: {min_name} (${minimum})")
+        if (month_count==0):            
+            month_count += 1
+            total = total + float(row[1])
+            log.append(float(row[1]))
+        else:
+            month_count += 1
+            total = total + float(row[1])
+            change = float(row[1])-log[-1]
+            print(change)
+            change_list.append(change)
+            if minimum == 0:
+                minimum = change
+                min_name = row[0]
+            if maximum == 0:
+                maximum = change
+                max_name = row[0]
+            elif change < minimum:
+                minimum = change
+                min_name = row[0]
+            elif change > maximum:
+                maximum = change
+                max_name = row[0]
+            log.append(float(row[1]))
 
-    greatest_incr = f"{max_name} (${maximum})"
-    greatest_decr = f"{min_name} (${minimum})"
-    results = {"Total Months": month_count, "Total": total, "Average Change": total/month_count, "Greatest Increase in Profits": greatest_incr, "Greatest Decrease in Profits": greatest_decr}
-    f = open("PyBank_results.txt", "w")
-    f.write(str(results))
-    f.close()
+print("Financial Analysis\n")
+print("--------------------")
+print(f"Total Months: {month_count}")
+print(f"Total: ${total}")
+print(f"Average  Change: ${round(sum(change_list)/len(change_list))}")
+print(f"Greatest Increase in Profits: {max_name} (${maximum})")
+print(f"Greatest Decrease in Profits: {min_name} (${minimum})")
+
+f = open("PyBank_results.txt", "w")
+f.write("Financial Analysis\n")
+f.write("----------------------\n")
+f.write(f"Total Months: {month_count}\n")
+f.write(f"Total: ${total}\n")
+f.write(f"Average  Change: ${round(sum(change_list)/len(change_list))}\n")
+f.write(f"Greatest Increase in Profits: {max_name} (${maximum})\n")
+f.write(f"Greatest Decrease in Profits: {min_name} (${minimum})\n")
+f.close()
